ea27f858a78bcee616e17dd1a3413d2130eca9b0
lord936 <b936755@gmail.com>
2026-02-23 11:44:20 +0530

    Refactor import statements and file path in ml.py

--- ml.py
+++ ml.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 
-df = pd.read_csv(r'C:\Users\b9367\Downloads\gpa_study_hours.csv')
+df = pd.read_csv('gpa_study_hours.csv')
 # here we read the csv file and store it in a variable called df
 x=df[['study_hours']]
 y=df['gpa']
@@ -12,4 +12,5 @@ model.fit(x,y)
 # here we fit the model to the data by calling the fit and passing x and y as arguments
 hours=float(input('enter study_hours :'))
 # we take input from the user and store it in avariable called hours and convert into float data type
+
 print(model.predict([[hours]]))
